24379b27fc90ab152787177aed25b07a864e96ca
Pratik Jha <84675755+pratikbee@users.noreply.github.com>
2022-06-19 16:19:25 +0530

    Update Screen1.js

--- components/Screen1.js
+++ components/Screen1.js
@@ -87,7 +87,7 @@ const getGenreBasedMovies=()=>{
         "method": "GET",
         "headers": {
           "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
-          "x-rapidapi-key": "ad8e26125bmsh7f8080efbc2506ap1f606bjsndb91972d2b0d"
+          "x-rapidapi-key": "<your key>"
         }
       });
       const data = await result.json();
